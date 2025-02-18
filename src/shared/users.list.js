80914aed377e2303e379f884d2a5f961a8e583b6
pouss707 <166956207+pouss707@users.noreply.github.com>
2025-02-18 14:49:02 +0100

    newupdate

--- src/shared/users.list.js
+++ src/shared/users.list.js
@@ -2,7 +2,8 @@ const UserList = [
         { id: 1, name: 'John Doe', email: 'john.doe@example.com', userType: 'admin' },
         { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com', userType: 'user' },
         { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com', userType: 'moderator' },
-        { id: 4, name: 'Bob Brown', email: 'bob.brown@example.com', userType: 'user' }
+        { id: 4, name: 'Bob Brown', email: 'bob.brown@example.com', userType: 'user' },
+        { id: 5, name: 'someone', email: 'someone@example.com', userType: 'admin' }
     ];
 
 
