e2bb882d658c7b4149cbbff14f8a9fd08ca1ba5a
pouss707 <166956207+pouss707@users.noreply.github.com>
2025-03-01 18:14:12 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
-
-
-
 import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router';
 
 
 function App() {
   return (
-    <Outlet />
+    <>
+      <Outlet />
+      <Formcomponent />
+
+    </>
   );
 }
 
